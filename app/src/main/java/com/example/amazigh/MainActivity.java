161c0234fd7342c9a291bad803c1785f128c8013
LeeflangQuinten8amo1 <65156293+LeeflangQuinten8amo1@users.noreply.github.com>
2020-11-24 15:58:01 +0100

    Splash screen aangemaakt

--- app/src/main/java/com/example/amazigh/MainActivity.java
+++ app/src/main/java/com/example/amazigh/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.amazigh;
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Handler;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent( MainActivity.this, SplashActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+
     }
 }
