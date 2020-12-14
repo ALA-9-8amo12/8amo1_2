98bfb0bb9856d8df28b58ad1b075f5be7dec522e
Dylan van Leeuwen <dylanvl@boodschapjes.com>
2020-12-14 23:09:03 +0100

    made catogorie page & oefenen page

---
+++ app/src/main/java/com/example/amazigh/CategorieModel.java
@@ -0,0 +1,17 @@
+package com.example.amazigh;
+public class CategorieModel {
+
+    private String Categorieën;
+    public CategorieModel(){}
+
+    public CategorieModel(String Categorieën){
+        this.Categorieën = Categorieën;
+    }
+
+    public String getCategorieën() {
+        return Categorieën;
+    }
+
+    public void setCategorieën(String Categorieën){this.Categorieën = Categorieën;}
+
+}
---
+++ app/src/main/java/com/example/amazigh/OefenenModel.java
@@ -0,0 +1,28 @@
+package com.example.amazigh;
+
+public class OefenenModel {
+    String Foto_bestandsnaam;
+    String Geluids_bestandsnaam;
+    String AMAZIGH;
+    String NL;
+
+    public OefenenModel(){}
+
+    public OefenenModel(String Foto_bestandsnaam, String Geluids_bestandsnaam, String NL, String AMAZIGH){
+        this.Foto_bestandsnaam = Foto_bestandsnaam;
+        this.Geluids_bestandsnaam = Geluids_bestandsnaam;
+        this.AMAZIGH = AMAZIGH;
+        this.NL = NL;
+    }
+
+    public String getFoto_bestandsnaam(){return Foto_bestandsnaam;}
+    public String getGeluids_bestandsnaam(){return Geluids_bestandsnaam;}
+    public String getAMAZIGH(){return AMAZIGH;}
+    public String getNL(){return NL;}
+
+    public void setFoto_bestandsnaam(String Foto_bestandsnaam) { this.Foto_bestandsnaam = Foto_bestandsnaam; }
+    public void setGeluids_bestandsnaam(String Geluids_bestandsnaam) { this.Geluids_bestandsnaam = Geluids_bestandsnaam; }
+    public void setAMAZIGH(String AMAZIGH) { this.AMAZIGH = AMAZIGH; }
+    public void setNL(String NL) { this.NL = NL; }
+
+}
